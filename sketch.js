4f1124e4996c9ea1bad8886cb14a549a9530731c
Arjun2000HP <84279486+Arjun2000HP@users.noreply.github.com>
2022-04-19 06:28:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,6 +8,17 @@ var non, nonimg;
 var message='HELLO WORLD';
 var respondent, returne, day, time, sdate, stime, date;
 var levnig,levnigimg;
+var ulcy, ulcyimg;
+var loadbox;
+var instabn, utubn;
+var likebn;
+var likecount=0;
+var overloaded, overloadimg;
+var bsod,bsodimg
+
+var deg = 12;
+var value = 0;
+
 
 function preload(){
 APITime();
@@ -15,6 +26,8 @@ APITime();
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+ // var onlyInSetup =5;
 
   morningimg=loadImage("morningimage.jpg");
   mrning=createSprite(windowWidth/2,320)
@@ -51,23 +64,87 @@ function setup() {
   levnig=createSprite(670,350)
   levnig.addImage(levnigimg)
   levnig.scale=0.5
-  levnig.visible=false
+  levnig.visible=false;
+  
+  ulcyimg=loadImage("Screenshot (4).png");
+  ulcy=createSprite(400,110)
+  ulcy.addImage(ulcyimg)
+  ulcy.scale=0.8;
+  ulcy.visible=false;
+
+  instabn=createButton('Instagram')
+  instabn.position(1270,55)
+  instabn.mousePressed(instagram);
+  instabn.hide()
+
+  utubn=createButton('YouTube')
+  utubn.position(1270,30);
+  utubn.mousePressed(utube);
+  utubn.hide()
+
+  likebn=createButton('Like')
+  likebn.position(470,580)
+  likebn.mousePressed(likef)
+
+  overloadimg=loadImage("burning.jpg");
+  overloaded=createSprite(400,190)
+  overloaded.addImage(overloadimg)
+  overloaded.scale=0.2;
+  overloaded.visible=false;
+
+  bsodimg=loadImage("something-went-wrong-error-3.png");
+  bsod=createSprite(700,320)
+  bsod.addImage(bsodimg)
+  bsod.scale=1.4;
+  bsod.visible=false;
+
 }
 
 function draw() {
   background(0,0,0);
-
   textFont('Courier New',20)
-  text("LOADING...",30,30);
+  text("LOADING. . .",600,windowHeight/2);
+  
+  fill (255,255,255);  
+  push();
+  translate (650,windowHeight/2-40);
+  rectMode (CENTER);
+  rotate (radians (deg));
+  rect(0,0,20,20);
+  pop();
+ deg+=750;
 
-  drawSprites();
+ drawSprites();
+ 
+ if(likecount>=59){
 
-  stroke('Black')
-  strokeWeight(4)
+  bsod.visible=true;
+ }
+
+ if(likecount>=61){
+  h 
+ }
+
+  if(likecount>=10){
+   stroke('RED')
+   strokeWeight(2)
+  text("THANKS For your Support",500,80)
+ }
+  
+ if(likecount>=50){
+   overload()
+   text("Please Refresh the page ",280,280)
+ }
+
+ text("Loving the Site? Do give us a Like", 50,600);
+ stroke('black')
+ text("Likes: "+ likecount,460,620)
+  stroke('Red')
+  strokeWeight(3)
   fill('White')
   text("DATE: "+sdate,windowWidth/2.5,20);
   text("TiME: "+stime,windowWidth/2.5,50);
-  
+
   //CHECK 1
   if(time>5 && time<12){
     console.log("check1ok");
@@ -75,12 +152,18 @@ function draw() {
     strokeWeight(2)
     fill(0,255,255)
     textFont('Tahoma',20)
-    text("Morning time is best to study",windowWidth/15,50)
-    text("Try learning something New from your Books today",windowWidth/15,75)
-    text("Have a healthy breakfast and Give your day healthy Start",windowWidth/15,100);
+    text("Good Morning ",620,250)
+    text("Forget the Past and Focus on today",620,275)
+    text("Just have a nice Bath and do a healthy breakfast to get a fresh Start ",620,300);
+    stroke('black')
+    strokeWeight(0.7)
+    fill("white")
+    textFont('Comic Sans MS',15)
+    text("Check back at 12:00:00 ",700,500);
   }
+  
   //CHECK 2
-  if(time>12 && time<16){
+  if(time>=12 && time<16){
     non.visible=true;
     console.log("check2ok");
     stroke('black')
@@ -88,10 +171,24 @@ function draw() {
     fill(255,255,255)
     textFont('Tahoma',20)
     text("Good Afternoon",windowWidth/15,75)
-    text("Afternoon time is short break time for your half of the day",windowWidth/15,100)
-    text("You can plan your further day at afternoon",windowWidth/15,125);
-    text("You can Watch T.V, check mails and take some rest At this time",windowWidth/15,150);
+    text("You can plan your further day at afternoon",windowWidth/15,100)
+    text("Check for any latest messages/mails at this time",windowWidth/15,125);
+    text("Feeling Sleepy After Lunch? Why not to do some Study",windowWidth/15,150);
+    text("A power Nap of 20mins can be taken at this time",windowWidth/15,175);
+    stroke('black')
+    strokeWeight(0.7)
+    fill("white")
+    textFont('Comic Sans MS',15)
+    text("Check back at 16:00:00",1000,475);
   }
+  
+  if(likecount>=55){
+    stroke('WHTIE')
+    strokeWeight(5)
+    textSize(50)
+    text("Get Out of Here N.O.W ",400,120);
+   }
+
   //CHECK 3
   if(time>=16 && time<17){
     erlyning.visible=true;
@@ -99,18 +196,19 @@ function draw() {
     strokeWeight(2)
     fill('black')
     textFont('Tahoma',20)
-    text("You can either just sleep and watch your dreams, or Wake up to chase your Dreams",350,120);
-    text("Life is 1 & Chance is 1, Stop being fool because opportunity is 1",450,170);
-    text("There's A very powerful book Bhagvat-Gita, try reading it ever!!",250,90);
-    stroke(0,0,0)
-    strokeWeight(3)
+    text("Good Evening",350,90);
+    text("Late evening is an excellent time to dedicate to yourself ",450,150);
+    text("Why not try something New today?",250,120);
+    stroke('black')
+    strokeWeight(0.7)
     fill("white")
-    textFont('Comic Sans MS',20)
+    textFont('Comic Sans MS',15)
     text("Check Back at 18:00:00",750,600);
     console.log("check3ok")
   }
+
   //CHECK 4
-  if(time>17 && time<=19){
+  if(time>17 && time<=19 ){
     evnig.visible=true;
     stroke('black')
     strokeWeight(2)
@@ -118,52 +216,138 @@ function draw() {
     textFont('Tahoma',20)
     text("Good Evening",windowWidth/15,50)
     text("Evening Time is Best to Spend with your Loved Ones",windowWidth/15,75)
-    text("If you are Alone like Me, Then Do something Creative This evening",windowWidth/15,100);
+    text("Go out and Play with friends or have a simple walk there to excercise the Body",windowWidth/15,100);
+    text("Laziness is your Biggest enemy, Keep your mobile down and GO OUT N.O.W !!",windowWidth/15,125);
+    stroke('black')
+    strokeWeight(0.7)
+    fill("white")
+    textFont('Comic Sans MS',15)
+    text("Check Back at 20:00:00",750,600);
     console.log("check4ok");
   } 
+  
   //CHECK 5
-  if(time>19 && time<22){
-    levnig.visible=true;
-        strokeWeight(2)
+  if(time>19 && time<=20){
+   levnig.visible=true;
+   strokeWeight(2)
         fill(0,255,255)
         textFont('Tahoma',20)
-        text("SO...I expect your homework is done by Now? If not then you must!",windowWidth/15,windowHeight/1.5)
-        text("Before Sleeping you must disconnect technology atleast 1hour early",windowWidth/15,windowHeight/1.4)
-        text("Why not Talk to your parents and discuss about your upcoming life",windowWidth/15,windowHeight/1.31);
+        text("So...How was the Day today? Note down your Good & Bad things in a diary",windowWidth/15,windowHeight/1.5)
+        text("Writing all your negativity on a paper and throwing it in dustbin reduces stress",windowWidth/15,windowHeight/1.4)
+        text("If you liked my Scheduling A.I, then do give your Review on Top Right ",windowWidth/15,windowHeight/1.31)
+        text("Early to Bed = Early to rise, So you must sleep before 10pm everyday",windowWidth/15,windowHeight/1.2);
+        stroke('black')
+        strokeWeight(0.7)
+        fill("white")
+        textFont('Comic Sans MS',15)
+        text("Check Back at 21:00:00",750,600);
     console.log("check5ok");
   }
+
   //CHECK 6
+  if(time>20 && time<22){
+     levnig.visible=true;
+         strokeWeight(2)
+         fill(0,255,255)
+         textFont('Tahoma',20)
+         text("Early to Bed = Early to rise, So you must sleep before 10pm everyday",windowWidth/15,windowHeight/1.5);
+         stroke('black')
+         strokeWeight(0.7)
+         fill("white")
+         textFont('Comic Sans MS',15)
+         text("Check Back at 22:00:00",750,600);
+     console.log("check6ok");
+   }
+
+  //CHECK 7
   if(time>=22 && time<24){
     nit.visible=true  
-    // stroke()
-    strokeWeight(2)
+    stroke('white')
+    strokeWeight(0.3)
     fill(0,255,255)
     textFont('Tahoma',20)
-    text("I knew you won't Sleep inspite of telling at 8pm",windowWidth/15,50)
-    text("This Moonlight reminds me of my friends whom I lost ",windowWidth/15,75)
-    text("I imagine myself with you at this place and forget all my tensions",windowWidth/15,100);
-    text("You believe it or not, I never worth you but I got you",windowWidth/15,125);
-    console.log("check6ok");
+    text("I knew you won't Sleep inspite of telling at 8pm, You never listen to me😒😒 ",windowWidth/15,150)
+    text("Late night wake isn't bad but after that early morning wake-up leds to Lack of rest ",windowWidth/15,175)
+    text("It is said that 20% of Brain still works at Sleep",windowWidth/15,200);
+    text("Your Internet will work faster after 10pm because of less devices online on your network.",windowWidth/15,225);
+    stroke('black')
+    strokeWeight(1.1)
+    fill("white")
+    textFont('Comic Sans MS',15)
+    text("Check back tomorrow",150,450);
+    console.log("check7ok");
   }
-}
 
+  if(time>16 && time<24 && date==19){
+   ulcy.visible=true;
+   textFont('forte',20)
+   text("Oh! ",225,305);
+   text("iT may be an unluck day for you Tomorrow?",225,325);
+   text("Be careful",225,345);
+   stroke('black')
+   strokeWeight(0.7)
+   fill("white")
+   textFont('Comic Sans MS',15)
+   text("Check back tomorrow",150,450);
+  }
+  
+  if(time<24){
+    stroke('blue')
+    strokeWeight(4)
+    text("Contact Us : ",1260,20)
+  }
+  
+}
 async function APITime(){
   respondent= await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
   returne= await respondent.json();
-   day= returne.datetime;
-   time=day.slice(11,13);
-   sdate=day.slice(2,10);
-   stime=day.slice(11,19);
+  day= returne.datetime;
+  time=day.slice(11,13);
+  sdate=day.slice(2,10);
+  stime=day.slice(11,19);
    date=day.slice(8,10);
-
+   
  if(time<12){
    console.log("GoodMorning")
- }
- else{
-   console.log("HELLO WORLD")
- }
+   contactext()
+  }
+
+  if(time>=12 && time<22){
+    console.log("HELLO WORLD")
+    contactext()
+  }
+
+  if(time>=22){
+    stroke('White')
+    fill('black')
+    text("Contact timings over, come tomorrow!!",1270,30)
+  }
   console.log(time);
   console.log(date)
   console.log("DisplayDate: "+sdate);
   console.log("DisplayTime: "+stime);
+}
+
+function instagram(){
+  window.open('https://www.instagram.com/gotecho/');
+}
+
+function utube(){
+  window.open('https://www.youtube.com/channel/UCwUvIxLa7JfdZwONzzoUJ2Q');
+}
+
+function contactext(){
+utubn.show()
+instabn.show()
+}
+
+function likef(){
+  likecount=likecount+1;
+}
+function overload(){
+  if(frameCount %12===0){
+    if(overloaded.visible===false){
+      overloaded.visible=true;
+    }else{overloaded.visible=false;}
+  }
 }
